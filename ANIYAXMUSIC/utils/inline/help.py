d3eb18d073c7ef8338da136b7a7a499d92507d87
SimpleXinno <surajperson767@gmail.com>
2026-07-24 14:45:53 +0530

    Update help.py

--- ANIYAXMUSIC/utils/inline/help.py
+++ ANIYAXMUSIC/utils/inline/help.py
@@ -1,64 +1,153 @@
 from typing import Union
+
 from ANIYAXMUSIC import app
-from ANIYAXMUSIC.utils.inline.start import api_btn
+from ANIYAXMUSIC.utils.inline.buttons import inline
 
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [api_btn(text=_["CLOSE_BUTTON"], callback_data="close", style="danger")]
-    second = [
-        api_btn(text=_["BACK_PAGE"], callback_data="mbot_cb", style="primary"),
-        api_btn(text=_["BACK_BUTTON"], callback_data="settingsback_helper", style="primary"),
-        api_btn(text=_["NEXT_PAGE"], callback_data="mbot_cb", style="success"),
+    first = [
+        inline._button(
+            text=_["CLOSE_BUTTON"],
+            callback_data="close",
+            category="danger",
+        )
     ]
+
+    second = [
+        inline._button(
+            text=_["BACK_PAGE"],
+            callback_data="mbot_cb",
+            category="primary",
+        ),
+        inline._button(
+            text=_["BACK_BUTTON"],
+            callback_data="settingsback_helper",
+            category="primary",
+        ),
+        inline._button(
+            text=_["NEXT_PAGE"],
+            callback_data="mbot_cb",
+            category="success",
+        ),
+    ]
+
     mark = second if START else first
-    upl = InlineKeyboardMarkup(
+
+    return inline.ikm(
         [
             [
-                api_btn(text=_["H_B_1"], callback_data="help_callback hb1", style="primary"),
-                api_btn(text=_["H_B_2"], callback_data="help_callback hb2", style="primary"),
-                api_btn(text=_["H_B_3"], callback_data="help_callback hb3", style="primary"),
+                inline._button(
+                    text=_["H_B_1"],
+                    callback_data="help_callback hb1",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_2"],
+                    callback_data="help_callback hb2",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_3"],
+                    callback_data="help_callback hb3",
+                    category="primary",
+                ),
             ],
             [
-                api_btn(text=_["H_B_4"], callback_data="help_callback hb4", style="primary"),
-                api_btn(text=_["H_B_5"], callback_data="help_callback hb5", style="primary"),
-                api_btn(text=_["H_B_6"], callback_data="help_callback hb6", style="primary"),
+                inline._button(
+                    text=_["H_B_4"],
+                    callback_data="help_callback hb4",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_5"],
+                    callback_data="help_callback hb5",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_6"],
+                    callback_data="help_callback hb6",
+                    category="primary",
+                ),
             ],
             [
-                api_btn(text=_["H_B_7"], callback_data="help_callback hb7", style="primary"),
-                api_btn(text=_["H_B_8"], callback_data="help_callback hb8", style="primary"),
-                api_btn(text=_["H_B_9"], callback_data="help_callback hb9", style="primary"),
+                inline._button(
+                    text=_["H_B_7"],
+                    callback_data="help_callback hb7",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_8"],
+                    callback_data="help_callback hb8",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_9"],
+                    callback_data="help_callback hb9",
+                    category="primary",
+                ),
             ],
             [
-                api_btn(text=_["H_B_10"], callback_data="help_callback hb10", style="primary"),
-                api_btn(text=_["H_B_11"], callback_data="help_callback hb11", style="primary"),
-                api_btn(text=_["H_B_12"], callback_data="help_callback hb12", style="primary"),
+                inline._button(
+                    text=_["H_B_10"],
+                    callback_data="help_callback hb10",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_11"],
+                    callback_data="help_callback hb11",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_12"],
+                    callback_data="help_callback hb12",
+                    category="primary",
+                ),
             ],
             [
-                api_btn(text=_["H_B_13"], callback_data="help_callback hb13", style="primary"),
-                api_btn(text=_["H_B_14"], callback_data="help_callback hb14", style="primary"),
-                api_btn(text=_["H_B_15"], callback_data="help_callback hb15", style="primary"),
+                inline._button(
+                    text=_["H_B_13"],
+                    callback_data="help_callback hb13",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_14"],
+                    callback_data="help_callback hb14",
+                    category="primary",
+                ),
+                inline._button(
+                    text=_["H_B_15"],
+                    callback_data="help_callback hb15",
+                    category="primary",
+                ),
             ],
             mark,
         ]
     )
-    return upl
 
 
 def help_back_markup(_):
-    upl = InlineKeyboardMarkup(
+    return inline.ikm(
         [
             [
-                api_btn(text=_["BACK_BUTTON"], callback_data="settings_back_helper", style="primary"),
+                inline._button(
+                    text=_["BACK_BUTTON"],
+                    callback_data="settings_back_helper",
+                    category="primary",
+                )
             ]
         ]
     )
-    return upl
 
 
 def private_help_panel(_):
-    buttons = [
+    return inline.ikm(
         [
-            api_btn(text=_["S_B_4"], url=f"https://t.me/{app.username}?start=help", style="success"),
-        ],
-    ]
-    return buttons
+            [
+                inline._button(
+                    text=_["S_B_4"],
+                    url=f"https://t.me/{app.username}?start=help",
+                    category="success",
+                )
+            ]
+        ]
+    )
